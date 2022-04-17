cb13d77ef3fbb92b8f14fd2e1855555ab4335991
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-04-17 08:45:05 -0700

    refactor pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { createContext, useState, useEffect, useContext } from 'react'
+import { AuthProvider } from '../hooks/useAuth'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Layout } from '../components/Layout'
 import { MORALIS_APP_ID, MORALIS_SERVER_URL } from '../constants'
-import { MoralisContextValue, MoralisProvider, useMoralis } from 'react-moralis'
+import { MoralisProvider } from 'react-moralis'
 import CssBaseline from '@mui/material/CssBaseline'
 import type { AppProps } from 'next/app'
 
@@ -11,43 +11,6 @@ const theme = createTheme({
     mode: 'dark',
   },
 })
-
-const AuthContext = createContext({ isConnected: false })
-
-const AuthProvider = ({ children }: IProps) => {
-  const moralisState: MoralisContextValue = useMoralis()
-  const {
-    isAuthenticated,
-    isWeb3Enabled,
-    account,
-    enableWeb3,
-    isWeb3EnableLoading,
-  } = moralisState
-  const [isConnected, setIsConnected] = useState<boolean>(false)
-  const shouldReEnableWeb3 =
-    isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading
-
-    useEffect(() => {
-    if (shouldReEnableWeb3) {
-      enableWeb3()
-    }
-  }, [shouldReEnableWeb3]) // eslint-disable-line
-
-  useEffect(() => {
-    if (isAuthenticated && account) {
-      setIsConnected(true)
-    } else {
-      setIsConnected(false)
-    }
-  }, [isWeb3Enabled, isAuthenticated, account]) // eslint-disable-line
-  return (
-    <AuthContext.Provider value={{ isConnected }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
-
-export const useAuth = () => useContext(AuthContext)
 
 function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,14 @@
-import { Button, Container } from '@mui/material'
-import type { NextPage } from 'next'
-import { useMoralis } from 'react-moralis'
+import { Container } from '@mui/material'
 import { FaucetForm } from '../components/FaucetForm'
-import { useAuth } from './_app'
+import { SplashScreen } from '../components/SplashScreen'
+import { useAuth } from '../hooks/useAuth'
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const { authenticate, isAuthenticated, logout } = useMoralis()
   const { isConnected } = useAuth()
 
-  const handleDisconnect = async () => {
-    await logout()
-  }
-
-  const handleConnectWallet = async () => {
-    if (!isAuthenticated) {
-      await authenticate({
-        signingMessage: 'Fweb3 faucet authentication',
-      })
-    }
-  }
-
-  const renderConnectButton = () => {
-    return <Button onClick={handleConnectWallet}>Connect Wallet</Button>
-  }
-
-  const renderLogoutButton = () => {
-    return <Button onClick={handleDisconnect}>Disconnect</Button>
-  }
-
   return (
-    <Container>
-      {isConnected ? renderLogoutButton() : renderConnectButton()}
-      <FaucetForm />
-    </Container>
+    <Container>{isConnected ? <FaucetForm /> : <SplashScreen />}</Container>
   )
 }
 
