3843d5c54f30e8e7b8843eb2957bd64de57113e5
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-04-22 08:19:13 -0700

    set alchemy provider

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,3 +1,4 @@
+import { AlchemyProvider } from "@ethersproject/providers"
 import { ethers } from "ethers"
 const { LOCAL_PRIVK, MUMBAI_PRIVK, POLYGON_PRIVK } = process.env
 
@@ -9,14 +10,18 @@ type Provider =
 export const getProvider = (network: string): Provider => {
   if (network !== 'local') {
     const netName = network === 'polygon' ? 'matic' : 'maticmum'
-    console.log(`infura provider: ${netName}`)
-    const provider = ethers.providers.getDefaultProvider(netName, {
-      infura: {
-        projectId: process.env.INFURA_PROJECT_ID,
-        projectSecret: process.env.INFURA_PROJECT_SECRET,
-      },
-      alchemy: process.env.ALCHEMY_API_KEY
-    })
+    console.log(`provider: ${netName}`)
+    // const provider = ethers.providers.getDefaultProvider(netName, {
+    //   infura: {
+    //     projectId: process.env.INFURA_PROJECT_ID,
+    //     projectSecret: process.env.INFURA_PROJECT_SECRET,
+    //   },
+    //   alchemy: process.env.ALCHEMY_API_KEY
+    // })
+    // const provider = ethers.providers.getDefaultProvider(netName, {
+    //   alchemy: process.env.ALCHEMY_API_KEY,
+    // })
+    const provider = new AlchemyProvider(netName, process.env.ALCHEMY_API_KEY)
     return provider
   }
   return new ethers.providers.JsonRpcProvider('http://localhost:8545')
