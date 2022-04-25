c5399489b343c3b1d5c6e6db7a142091570d9910
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-04-24 21:09:33 -0700

    maintenance message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import { FaucetForm } from '../components/Faucet/FaucetForm'
 import { SplashScreen } from '../components/SplashScreen'
 import { useAuth } from '../hooks/useAuth'
@@ -7,8 +7,14 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   const { isConnected } = useAuth()
+  // return (
+  //   <Container>{isConnected ? <FaucetForm /> : <SplashScreen />}</Container>
+  // )
   return (
-    <Container>{isConnected ? <FaucetForm /> : <SplashScreen />}</Container>
+    <Container>
+      <Typography variant='h3'>Sorry. The faucet is down for maintenance.</Typography>
+      <Typography variant='h4'>Please check back tomorrow</Typography>
+    </Container>
   )
 }
 
